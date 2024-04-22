56705bcf85295d7a6a7880e723e13dd3437015b5
Khatami,Hamid <khatami@caspco.ir>
2024-04-22 10:38:06 +0330

    styling

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import styles from "../app/ui/home.module.css";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Next
+    // These are Tailwind classes:
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"></div>
+      <p>Hello Next</p>
+      <h1 className="text-blue-500">I'm blue!</h1>
+      <div className={styles.shape} />
     </main>
   );
 }
