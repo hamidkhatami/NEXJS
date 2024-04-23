0ff0635802fa7c3f9378a6af46c69aa5ccccb3bd
Khatami,Hamid <khatami@caspco.ir>
2024-04-23 17:08:55 +0330

    font

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../app/ui/globals.css";
-import { Roboto } from "next/font/google";
+import { Inter, Roboto_Mono, Roboto } from "next/font/google";
 import { inter } from "../app/ui/fonts";
 
 const inter2 = Inter({
@@ -17,6 +15,17 @@ const roboto = Roboto({
   display: "swap",
 });
 
+const inter3 = Inter({
+  subsets: ["latin"],
+  variable: "--a",
+  display: "swap",
+});
+const roboto_mono3 = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,7 +35,13 @@ export default function RootLayout({
     <html lang="en">
       {/* Layout UI */}
       {/* <body className={inter.className}>{children}</body> */}
-      <body className={inter.className}>{children}</body>
+      <body>
+        {/* <div className={inter.className}>{children}</div>
+        <div className={roboto.className}>{children}</div> */}
+        <div className={`${inter3.variable} ${roboto_mono3.variable}`}>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,27 +1,21 @@
 import Image from "next/image";
-
+import styles from "../app/ui/global.css";
+import "../app/ui/home.module.css";
 import { Inter, Roboto_Mono } from "next/font/google";
-import "../app/ui/globals.css";
 
 export default function Home() {
-  const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-inter",
-    display: "swap",
-  });
-  const roboto_mono = Roboto_Mono({
-    subsets: ["latin"],
-    variable: "--font-roboto-mono",
-    display: "swap",
-  });
   return (
     // These are Tailwind classes:
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"></div>
+    // <main className="flex min-h-screen flex-col p-6">
+    //   <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"></div>
+    <div>
       <p>Hello Next</p>
       <h1 className="text-blue-500">I'm blue!</h1>
-      <div className={styles.shape} />
-      <div className={roboto_mono.className}> italic</div>
-    </main>
+      <div className={styles.shape}>style shape</div>
+      <br />
+      {/* <div className={roboto_mono.className}> italic</div> */}
+
+      {/* <div className={`${inter.variable} ${roboto_mono.variable}`}>sss</div> */}
+    </div>
   );
 }
